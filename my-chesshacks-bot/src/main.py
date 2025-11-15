186c90db3667cc4c7d66cb2076fe6d08ca920b7d
Ian Ludanik <67395710+ludanik@users.noreply.github.com>
2025-11-15 12:34:13 -0500

    cleanup

--- my-chesshacks-bot/src/main.py
+++ my-chesshacks-bot/src/main.py
@@ -12,12 +12,6 @@ model = AutoModel.from_pretrained("Maxlegrec/ChessBot", trust_remote_code=True)
 device = "cpu"
 model = model.to(device)
 
-def get_move(pgn: str) -> str:
-    # Your bot logic here
-    # Receives board state as PGN
-    # Returns move in UCI format
-    pass
-
 @chess_manager.entrypoint
 def test_func(ctx: GameContext):
     # This gets called every time the model needs to make a move
@@ -32,36 +26,18 @@ def test_func(ctx: GameContext):
         ctx.logProbabilities({})
         raise ValueError("No legal moves available (i probably lost didn't i)")
 
-
-    # my code
-    # need to get fen from pgn
     fen = ctx.board.fen()
     probs = model.get_move_from_fen_no_thinking(fen, T=1, device=device, return_probs=True)
     top_moves = sorted(probs.items(), key=lambda x: x[1], reverse=True)[:5]
-    '''
-    
-    '''
-    move_probs = top_moves
-    print("my model")
-    print(move_probs)
-    print(probs)
-    print(top_moves)
-    ctx.logProbabilities(move_probs)
-
-    move_weights = [random.random() for _ in legal_moves]
-    total_weight = sum(move_weights)
+ 
     # Normalize so probabilities sum to 1
     move_probs = {
         Move.from_uci(move): weight
         for move, weight in top_moves
     }
     ctx.logProbabilities(move_probs)
-    print("random weights")
-    
-    print(move_probs)
-    print(move_weights)
+
     return Move.from_uci(top_moves[0][0])
-    #return random.choices(legal_moves, weights=move_weights, k=1)[0]
 
 
 
