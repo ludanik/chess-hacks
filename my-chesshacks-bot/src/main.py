27d1e83cb2d5bcf773811c8339b248074d017218
Ian Ludanik <67395710+ludanik@users.noreply.github.com>
2025-11-15 11:49:32 -0500

    try with pre-made model first

--- my-chesshacks-bot/src/main.py
+++ my-chesshacks-bot/src/main.py
@@ -5,7 +5,18 @@ import time
 
 # Write code here that runs once
 # Can do things like load models from huggingface, make connections to subprocesses, etcwenis
+import torch
+from transformers import AutoModel
 
+model = AutoModel.from_pretrained("Maxlegrec/ChessBot", trust_remote_code=True)
+device = "cpu"
+model = model.to(device)
+
+def get_move(pgn: str) -> str:
+    # Your bot logic here
+    # Receives board state as PGN
+    # Returns move in UCI format
+    pass
 
 @chess_manager.entrypoint
 def test_func(ctx: GameContext):
@@ -21,6 +32,13 @@ def test_func(ctx: GameContext):
         ctx.logProbabilities({})
         raise ValueError("No legal moves available (i probably lost didn't i)")
 
+
+    # my code
+    # need to get fen from pgn
+    fen = ctx.board.fen()
+    probs = model.get_move_from_fen_no_thinking(fen, T=1, device=device, return_probs=True)
+    top_moves = sorted(probs.items(), key=lambda x: x[1], reverse=True)[:5]
+    '''
     move_weights = [random.random() for _ in legal_moves]
     total_weight = sum(move_weights)
     # Normalize so probabilities sum to 1
@@ -29,8 +47,13 @@ def test_func(ctx: GameContext):
         for move, weight in zip(legal_moves, move_weights)
     }
     ctx.logProbabilities(move_probs)
+    '''
+    move_probs = top_moves
+    print(move_probs)
+    ctx.logProbabilities(move_probs)
+    return random.choices(legal_moves, weights=move_probs, k=1)[0]
 
-    return random.choices(legal_moves, weights=move_weights, k=1)[0]
+    #return random.choices(legal_moves, weights=move_weights, k=1)[0]
 
 
 @chess_manager.reset
